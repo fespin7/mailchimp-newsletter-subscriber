795ef5e5d36f97a644c766a58577fdb90545937c
fespin7 <paulesp7@gmail.com>
2023-08-14 23:41:57 -0500

    Fixed use of api key, now it is on env variable

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded());
 
 const port = process.env.PORT || 80;
-// const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
+const listId = process.env.LIST_ID;
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
@@ -17,6 +18,8 @@ app.post("/", function (req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
+
+  console.log("Api Key: ", apiKey);
 
   const memberData = {
     members: [
@@ -36,11 +39,11 @@ app.post("/", function (req, res) {
   // URL
   // https://us21.api.mailchimp.com/3.0/lists/{list_id}
 
-  const url = "https://us21.api.mailchimp.com/3.0/lists/313255e752";
+  const url = `https://us21.api.mailchimp.com/3.0/lists/${listId}`;
 
   const options = {
     method: "POST",
-    auth: "key1:16e47b4b33926b83d7858653e65d5bdc-us21",
+    auth: `key1:${apiKey}`,
   };
 
   const apiRequest = https.request(url, options, (apiResponse) => {
@@ -74,5 +77,5 @@ app.post("/failure", function (req, res) {
 });
 
 app.listen(port, function () {
-  console.log("App listen on port 3001");
+  console.log(`App listen on port ${port}`);
 });
